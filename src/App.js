284bdcc7e8b208e6f4479eb2d96da6dd711cbcf4
Mohammed NIhal v K <mohammednihal691@gmail.com>
2023-11-30 20:52:14 +0530

    alert

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 import { useSelector } from "react-redux";
+import Notification from "./components/Notification";
 
 function App() {
-  const isLoggedIn = useSelector(state=>state.auth.isLoggedIn);
+  const cart = useSelector(state => state.cart);
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
   console.log(isLoggedIn);
+  useEffect(() => {
+    const sendRequest = async () => {
+      const res = await fetch('https://redux-shoping-test-default-rtdb.firebaseio.com/cartItems.json',
+        {
+          method: "PUT",
+          body: JSON.stringify(cart)
+        }
+      );
+      const data = await res.json();
+    }
+    sendRequest()
+
+  }, [cart])
   return (
     <div className="App">
+      <Notification type='success' message={'this is dummy'}/>
       {!isLoggedIn && < Auth />}
-     {isLoggedIn &&  <Layout />}
+      {isLoggedIn && <Layout />}
     </div>
   );
 }
